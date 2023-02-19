8fa61c3e58e33cc0bb038e26afaf7e27cc2a225f
Daiven Reyes <98158666+Devriex@users.noreply.github.com>
2023-02-19 05:49:32 +0000

    added gallery

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+GALLERY = [
+  {
+    'id': 1,
+    'title' : '1 designs',
+    'imgurl' : '#'
+  },
+  {
+    'id': 2,
+    'title' : '2 designs',
+    'imgurl' : '#'
+  },
+  {
+    'id': 3,
+    'title' : '3 designs',
+    'imgurl' : '#'
+  },
+  {
+    'id': 4,
+    'title' : '4 designs',
+    'imgurl' : '#'
+  },
+  {
+    'id': 5,
+    'title' : '5 designs',
+    'imgurl' : '#'
+  },
+  
+]
 
 
 @app.route("/")
 def hello_world():
   return render_template('home.html')
 
+
+@app.route("/api/gallery")
+def listgallery():
+  return jsonify(GALLERY)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
